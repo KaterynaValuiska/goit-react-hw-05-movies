f77926e14762f7bbaec4ad68f123cace1351442b
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-08-10 10:41:45 +0200

    444

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,8 +7,13 @@ export const Layout = () => {
   return (
     <div className="Layout">
       <nav className="LayoutNav">
-        <NavLink to="/">Home</NavLink> {}
-        <NavLink to="/movies">Movies</NavLink>
+        <NavLink to="/" className="NavLink">
+          Home
+        </NavLink>{' '}
+        {}
+        <NavLink to="/movies" className="NavLink">
+          Movies
+        </NavLink>
         <img
           src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_1-8ba2ac31f354005783fab473602c34c3f4fd207150182061e425d366e4f34596.svg"
           alt=""
